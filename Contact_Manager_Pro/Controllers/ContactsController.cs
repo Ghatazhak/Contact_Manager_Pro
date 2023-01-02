9ed4beba860c7e46f197e400f12c599daed643cb
Bart Lyons <lyons.bart@protonmail.com>
2023-01-02 09:32:04 -0500

    Fixed delete view and action

--- Contact_Manager_Pro/Controllers/ContactsController.cs
+++ Contact_Manager_Pro/Controllers/ContactsController.cs
@@ -340,9 +340,11 @@ namespace Contact_Manager_Pro.Controllers
                 return NotFound();
             }
 
+            string appUserId = _userManager.GetUserId(User);
+
             var contact = await _context.Contacts
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
+                .FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
+
             if (contact == null)
             {
                 return NotFound();
@@ -357,17 +359,17 @@ namespace Contact_Manager_Pro.Controllers
         [Authorize]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Contacts == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Contacts'  is null.");
-            }
-            var contact = await _context.Contacts.FindAsync(id);
+
+            string appUserId = _userManager.GetUserId(User);
+
+            var contact = await _context.Contacts.FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
+
             if (contact != null)
             {
                 _context.Contacts.Remove(contact);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
