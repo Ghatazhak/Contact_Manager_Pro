f6ed345cb065a71a196d5c9455977bb0cf8057e6
Bart Lyons <lyons.bart@protonmail.com>
2023-01-11 23:50:46 -0500

    Setup two factor authorization.

--- Contact_Manager_Pro/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Contact_Manager_Pro/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -181,7 +181,7 @@ namespace Contact_Manager_Pro.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("CM Pro"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
